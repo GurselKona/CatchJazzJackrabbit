8935059b7cfed903ff45ba657f2b2200ef3b16e8
Gürsel Kona <gursel.kona@gmail.com>
2023-11-22 12:49:51 +0300

    new version

--- main.py
+++ main.py
@@ -1,72 +1,88 @@
-import random
 import turtle
-import time
+import random
+
 
 score = 0
 timer = 10
+game_over = False
+
+FONT = ("arial", 30, "normal")
+
+screen = turtle.Screen()
+screen.bgcolor("green")
+screen.title("Green Green Grassland!")
+score_pen = turtle.Turtle()
+score_pen.hideturtle()
+countdown_pen = turtle.Turtle()
+countdown_pen.hideturtle()
+jack = turtle.Turtle()
+jack.hideturtle()
+jack.penup()
+jack.shape("turtle")
+jack.shapesize(2)
+max_width = int(screen.window_width() / 2 * 0.75)
+max_height = int(screen.window_height() / 2 * 0.75)
+jack.speed("fastest")
+
+
+def setup_score_pen():
+
+    score_pen.color("yellow")
+    score_pen.hideturtle()
+    score_pen.penup()
+    height = screen.window_height() / 2
+    score_pen.goto(0, height * 0.9)
+    score_pen.clear()
+    score_pen.write(f"Score: {score}", align="center", font=FONT)
+
+
+def countdown(timer):
+    global game_over
+    countdown_pen.color("yellow")
+    countdown_pen.hideturtle()
+    countdown_pen.penup()
+    height = screen.window_height() / 2
+    countdown_pen.goto(0, height * 0.80)
+
+    if timer > 0:
+        countdown_pen.clear()
+        countdown_pen.write(f"Timer: {timer}", align="center", font=FONT)
+        screen.ontimer(lambda: countdown(timer-1),1000)
+    else:
+        game_over = True
+        countdown_pen.clear()
+        countdown_pen.write("G a m e  O v e r !", align="center", font=FONT)
 
 
 def is_catch(x, y):
-    tolerance = 15
     global score
-    if ((abs(x - jack.pos()[0]) <= tolerance)
-            and (abs(y - jack.pos()[1]) <= tolerance)):
-        score += 1
-        pen3.clear()
-        pen4.clear()
+    score += 1
+    score_pen.clear()
+    score_pen.write(f"Score: {score}", align="center", font=FONT)
 
-meadow = turtle.Screen()
-meadow.bgcolor("green")
-meadow.title("Green Green Grasslands!")
-meadow.setup(500,500)
-jack = turtle.Turtle("turtle")
-jack.penup()
-pen1 = turtle.Turtle()
-pen2 = turtle.Turtle()
-pen3 = turtle.Turtle()
-pen4 = turtle.Turtle()
-pen1.pencolor("yellow")
-pen1.hideturtle()
-pen1.penup()
-pen1.goto(-100, 230)
-pen1.pendown()
-pen1.write("Score: ", font=("arial", 14, "bold"))
-pen2.pencolor("yellow")
-pen2.hideturtle()
-pen2.penup()
-pen2.goto(-100, 210)
-pen2.pendown()
-pen2.write("Timer: ", font=("arial", 14, "bold"))
-pen3.pencolor("yellow")
-pen3.hideturtle()
-pen3.penup()
-pen3.goto(0, 230)
-pen4.pencolor("yellow")
-pen4.hideturtle()
-pen4.penup()
-pen4.goto(0, 210)
 
-while timer >= 0:
-    pen3.clear()
-    pen4.clear()
-    pen3.pendown()
-    pen4.pendown()
-    pen3.write(score, font=("arial", 14, "bold"))
-    pen4.write(timer, font=("arial", 14, "bold"))
-    jack.goto(random.randint(-200, 200), random.randint(-200, 200))
-    time.sleep(1)
-    timer -= 1
-    meadow.onclick(is_catch)
-pen2.clear()
-pen4.clear()
-pen5 = turtle.Turtle()
-pen5.pencolor("black")
-pen5.hideturtle()
-pen5.penup()
-pen5.goto(-120, 200)
-pen5.pendown()
-pen5.write("G a m e  O v e r!", font=("arial", 20, "bold"))
-turtle.done()
+def jumping_jack():
+    global game_over
+    jack.goto(random.randint(-max_width, max_height),
+              random.randint(-max_width, max_height))
+    jack.showturtle()
+    if not game_over:
+        screen.ontimer(jumping_jack,800)
+        jack.onclick(is_catch)
+    else:
+        jack.hideturtle()
+
+
+def begin():
+    global game_over
+    game_over = False
+    setup_score_pen()
+    jumping_jack()
+    countdown(timer)
+
+
+begin()
+turtle.mainloop()
 
 
 
